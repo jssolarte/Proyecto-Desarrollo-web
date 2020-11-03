3c3b803949c5654b01c49ebaae0fa8f6c2cd4727
Johan Steven Solarte <johan.solarte@uao.edu.co>
2020-11-03 09:14:56 -0500

    Pantalla home

--- grandparents/src/Components/Home/home.js
+++ grandparents/src/Components/Home/home.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import "./home.css"
+function Home(){
+    return(
+        <div className="o-container-home">
+            <nav className="o-nav-home">
+              <img className="o-img-home"
+                src="../.././logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+                <ul className="o-ul-home">
+                    <li className="o-li-home">Inicio</li>
+                    <li className="o-li-home">Actividades</li>
+                    <li className="o-li-home">Testimonios</li>
+                    <li className="o-li-home">Conócenos</li>
+
+                </ul>
+          </nav>
+        </div>
+    );
+}
+export default Home;
