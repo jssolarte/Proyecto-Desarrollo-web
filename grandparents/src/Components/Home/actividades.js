4963a03ccff9f8357a81d3d5dc94c297396bec09
Johan Steven Solarte <johan.solarte@uao.edu.co>
2020-11-03 21:01:48 -0500

    correcciones rutas

--- grandparents/src/Components/Home/actividades.js
+++ grandparents/src/Components/Home/actividades.js
@@ -16,7 +16,30 @@ function Actividades(){
   
   <Switch>
   <Route path="/actividades">
-        <h1>Hola mundooo</h1>
+  <div>
+  <nav className="o-nav-home">
+  <div className="o-nav-left">
+   <img className="o-img-home"
+     src={imglogo}
+     width="35"
+     height="35"
+     className="d-inline-block align-top"
+     alt="React Bootstrap logo"
+      />
+    <p>GrandParents Enjoy</p>
+  </div>  
+   <div className="o-nav-right">
+    <ul className="o-ul-home">
+      <Link to="/" className="o-li-home">Inicio</Link>
+      <Link to="/actividades" className="o-li-home">Actividades</Link>
+      <Link className="o-li-home">Testimonios</Link>
+      <Link className="o-li-home">Conócenos</Link>
+    </ul>
+   </div>
+</nav>
+</div>
+
+<div> aqui debe ir el contenido para actividades</div>
   </Route>
   </Switch>
         </Router>
